ce18a54fbbf015d3c269f2dfda039c674e484236
YannParis42 <yann.paris.42@gmail.com>
2022-07-11 16:56:42 +0200

    commit

--- src/Repository/CommandRepository.php
+++ src/Repository/CommandRepository.php
@@ -136,32 +136,4 @@ class CommandRepository extends ServiceEntityRepository
         ->getQuery()->getOneOrNullResult();
     }
    
-    // /**
-    //  * @return Command[] Returns an array of Command objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('c.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Command
-    {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
